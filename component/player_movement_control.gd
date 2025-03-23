1b18a107347e1faa0960dc505f64eab5bc7d6836
lilkaz <39888536+lilkaz@users.noreply.github.com>
2025-03-23 00:13:34 -0400

    player_dodge
    player is able to dodge

--- component/player_movement_control.gd
+++ component/player_movement_control.gd
@@ -2,7 +2,19 @@ class_name PlayerMovementControl extends Node
 
 @export var body: CharacterBody2D;
 @export var collision: CollisionObject2D;
-	
+
+
+#Dodge variables
+@export var dodge_speed = 200
+@export var dodge_duration = 0.2
+@export var dodge_cooldown = 1.0
+
+var dodge_timer = 0.0
+var cooldown_timer = 0.0
+var is_dodging = false
+var dodge_direction = Vector2.ZERO
+
+
 func _enter_tree() -> void:
 	assert(owner is Entity);
 	owner.set_meta(&"PlayerMovementControl", self);
@@ -15,8 +27,37 @@ func _physics_process(delta: float) -> void:
 	if (!(owner as Entity).has_component("Movement")):
 		return;
 	var movement = (owner as Entity).get_component("Movement") as Movement;
-	movement.move(direction);
+	
+	
+	#Dodge handling (SPACE BAR)
+	if is_dodging:
+		perform_dodge(movement, delta)
+		return
+
+	if Input.is_key_pressed(KEY_SPACE) and cooldown_timer <= 0:
+		start_dodge(direction)
 		
+	
+	movement.move(direction);
+	
+	#Dodge cooldown handle
+	if cooldown_timer > 0:
+		cooldown_timer -= delta
+	
+	
+func start_dodge(direction: Vector2) -> void:
+	is_dodging = true
+	dodge_timer = dodge_duration
+	cooldown_timer = dodge_cooldown
+	dodge_direction = direction	
+	
+func perform_dodge(movement: Movement, delta: float) -> void:
+	movement.move(dodge_direction * dodge_speed * delta)
+	dodge_timer -= delta
+	if dodge_timer <= 0:
+		is_dodging = false
+	
+
 func getInputDirection() -> Vector2:
 	var direction = Vector2.ZERO;
 	if Input.is_key_pressed(KEY_W):
@@ -32,3 +73,4 @@ func getInputDirection() -> Vector2:
 		return direction.normalized();
 	else:
 		return Vector2.ZERO;
+	
